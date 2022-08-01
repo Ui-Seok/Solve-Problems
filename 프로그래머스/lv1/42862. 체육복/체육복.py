61c08d0087eb67b391008cf637f33278f8a32631
Seok <82555245+Ui-Seok@users.noreply.github.com>
2022-08-02 02:19:56 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -2,6 +2,7 @@ def solution(n, lost, reserve):
     answer = 0
     students = {}
     
+    # 현재 옷을 가지고 있는 학생들의 dictionary
     for i in range(n):
         students[i + 1] = 1
     for l in lost:
@@ -9,8 +10,9 @@ def solution(n, lost, reserve):
     for r in reserve:
         students[r] += 1
         
+    # 학생이 옷을 2벌 가지고 있으면서 양옆의 학생이 옷이 없을때 주는 조건
     for student, cloth in students.items():
-        if student == 1 and cloth == 2:
+        if student == 1 and cloth == 2: 
             if students[2] == 0:
                 students[2] += 1
                 students[1] -= 1
@@ -25,7 +27,8 @@ def solution(n, lost, reserve):
             elif students[student + 1] == 0:
                 students[student + 1] += 1
                 students[student] -= 1
-            
+                
+    # 옷을 가지고 있는 학생의 숫자 카운트
     for _, i in students.items():
         if i != 0:
             answer += 1
