c03bd2cb414dc1e398b52da41cacf0ba937e23f7
Seok <82555245+Ui-Seok@users.noreply.github.com>
2022-08-15 20:08:02 +0900

    Update 통계학.py

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,18 +1,23 @@
 n = int(input())
 arr = [int(input()) for _ in range(n)]
 
+# 산술평균
 def average(arr):
     answer = sum(arr) / len(arr)
+    # 음수일때의 반올림 구하기
     if answer < 0:
         return int(round(answer))
+    # 양수일때의 반올림 구하기 -> 2.5일때 3으로 반올림 해주는 코드
     else:
         return int(answer) + 1 if (answer - int(answer)) >= 0.5 else int(answer)
 
+# 중앙값
 def median(arr):
     arr.sort()
     med = len(arr) // 2
     return arr[med]
 
+# 최빈값
 def many(arr):
     if len(arr) == 1:
         return arr[0]
@@ -21,21 +26,26 @@ def many(arr):
         maxcount = 0
         an_cnt = 0
         for i in arr:
+            # 딕셔너리에 있을때 count 1 추가
             if count.get(i) != None:
                 count[i] += 1
+            # 딕셔너리가 없을때 count 만들기
             else:
                 count[i] = 1
         for j in count:
             if count[j] > maxcount:
+            # 전체적으로 돌면서 maxcount 업데이트
                 maxcount = count[j]
                 answer = j
                 an_cnt = 0
             elif count[j] == maxcount:
+                # maxcount가 나오고 한번 더 나올시 멈춰주는 코드 -> 두번째로 작은 값
                 if an_cnt == 0 and answer < j:
                     answer = j
                     an_cnt += 1
         return answer
 
+# 범위
 def ranges(arr):
     return arr[-1] - arr[0]
 
