b3075e2a518245e5f811a83c4218f88c987b2505
Seok <82555245+Ui-Seok@users.noreply.github.com>
2022-08-16 18:55:27 +0900

    [level 1] Title: 폰켓몬, Time: 0.35 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,11 +1,13 @@
 '''
 시작시간: 18시 43분
-종료시간: 
+종료시간: 18시 54분
 '''
 
 def solution(nums):
     answer = 0
+    # 중복 제거하기
     arr = set(nums)
+    # 중복제거 후 리스트가 nums의 1/2보다 크면 nums의 1/2, 작으면 중복이 제거된 리스트의 길이
     if len(arr) > (len(nums) // 2):
         answer = len(nums) // 2
     else:
