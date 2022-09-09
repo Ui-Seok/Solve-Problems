eaf21d4cb9d755e2038cf6d5a62ba0b482cf13d1
Seok <82555245+Ui-Seok@users.noreply.github.com>
2022-09-10 01:44:27 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 61.84 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -11,13 +11,12 @@ def solution(participant, completion):
         part[i] = 0
     for i in participant:
         part[i] += 1
-    
     for i in completion:
         part[i] -= 1
     
     items = part.items()
     for item in items:
         if item[1] == 1:
-            answer += item[0]
-    
+            answer += item[0]    
+            
     return answer
