4c7326c92ed84bdf614424ed5b77b84100cf9721
Seok <82555245+Ui-Seok@users.noreply.github.com>
2022-09-17 23:08:52 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -2,8 +2,6 @@ def solution(array, commands):
     answer = []
     for command in commands:
         a, b, c = command
-        new_arr = array[a - 1 : b]
-        new_arr.sort()
+        new_arr = sorted(array[a - 1 : b])
         answer.append(new_arr[c - 1])
-        
     return answer
