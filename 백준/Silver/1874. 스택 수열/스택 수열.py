7e0f86a78e78a34de1500338efd7d1f3f55a6fe0
Seok <82555245+Ui-Seok@users.noreply.github.com>
2023-02-16 03:27:08 +0900

    [Silver II] Title: 스택 수열, Time: 3972 ms, Memory: 36696 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,36 +1,22 @@
 n = int(input())
-answer_list = [int(input()) for _ in range(n)]
-numbers = [i for i in range(1, n+1)]
-stack = list()
+check_arr = [int(input()) for _ in range(n)]
+stack_arr = [0]
+answer = list()
 
-pop_cnt = 0
-push_pop_list = list()
+idx = 0
+st_num = 1
+while idx < n and st_num <= n+1:
+    if stack_arr[-1] != check_arr[idx]:
+        stack_arr.append(st_num)
+        st_num += 1
+        answer.append('+')
+    else:
+        stack_arr.pop()
+        idx += 1
+        answer.append('-')
 
-cnt = 0
-while cnt <= 200001:
-    if stack and stack[-1] == answer_list[0]:
-        pop_cnt += 1
-        push_pop_list.append(1) # pop
-        del answer_list[0]
-        del stack[-1]
-        
-    else:    
-        if numbers and numbers[0] == answer_list[0]:
-            pop_cnt += 1    # count pop
-            push_pop_list.append(0) # push
-            push_pop_list.append(1) # pop
-            del numbers[0]
-            del answer_list[0]
-        elif numbers:
-            push_pop_list.append(0) # push
-            stack.append(numbers[0])
-            del numbers[0]
-
-    cnt += 1
-
-if pop_cnt != n:
+if stack_arr != [0]:
     print('NO')
 else:
-    for i in push_pop_list:
-        if not i: print('+')
-        else: print('-')
+    for i in answer:
+        print(*i)
