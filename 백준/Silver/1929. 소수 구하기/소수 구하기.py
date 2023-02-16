0ac7b2716adf6d0aeb9fd4bb295acb61f6e97d46
Seok <82555245+Ui-Seok@users.noreply.github.com>
2023-02-17 03:45:29 +0900

    [Silver III] Title: 소수 구하기, Time: 3196 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,10 +1,15 @@
-m,n=map(int,input().split())
+m, n = map(int, input().split())
 
-for num in range(m,n+1):
-    if num == 1:
-        continue
-    for i in range(2,int(num**0.5)+1):
+# 소수 판별 코드
+def check_prime(num):
+    root = int(num ** (1/2))
+    for i in range(2, root + 1):
         if num % i == 0:
-            break
-    else:
-        print(num)
+            return False
+    return True
+
+for i in range(m, n+1):
+    if i == 1:
+        continue
+    if check_prime(i):
+        print(i)
