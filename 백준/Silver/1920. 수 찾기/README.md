92b8d7c95f2b1fca7929f1cbcfc7d38baa7d97a8
Seok <82555245+Ui-Seok@users.noreply.github.com>
2023-02-17 02:41:21 +0900

    [Silver IV] Title: 수 찾기, Time: 492 ms, Memory: 46740 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/README.md
+++ 백준/Silver/1920. 수 찾기/README.md
@@ -4,11 +4,11 @@
 
 ### 성능 요약
 
-메모리: 46416 KB, 시간: 704 ms
+메모리: 46740 KB, 시간: 492 ms
 
 ### 분류
 
-이분 탐색(binary_search), 자료 구조(data_structures)
+이분 탐색(binary_search), 자료 구조(data_structures), 정렬(sorting)
 
 ### 문제 설명
 
--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,24 +1,24 @@
 n = int(input())
-answer_list = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 m = int(input())
-check_list = list(map(int, input().split()))
+check_arr = list(map(int, input().split()))
 
-answer_list.sort()
+arr.sort()
 
-def binary_search(data, start, end, target):
+def binary_search(arr, start, end, target):
     if start > end:
         return None
     mid = (start + end) // 2
-    if data[mid] == target:
+    if arr[mid] == target:
         return target
-    elif data[mid] > target:
-        return binary_search(data, start, mid-1, target)
+    elif arr[mid] > target:
+        return binary_search(arr, start, mid-1, target)
     else:
-        return binary_search(data, mid+1, end, target)
+        return binary_search(arr, mid+1, end, target)
 
-for i in check_list:
-    ch = binary_search(answer_list, 0, n-1, i)
-    if ch == i:
+for i in check_arr:
+    target = binary_search(arr, 0, n-1, i)
+    if target == i:
         print(1)
     else:
         print(0)
