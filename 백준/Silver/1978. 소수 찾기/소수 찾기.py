2daf8f4040532d92d2855d16b225dbe04bbb589a
Seok <82555245+Ui-Seok@users.noreply.github.com>
2023-02-22 02:41:03 +0900

    [Silver V] Title: 소수 찾기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -1,19 +1,16 @@
 n = int(input())
 arr = list(map(int, input().split()))
+
+def check_prime(num):
+    sqrt_num = num ** (1/2)
+    for i in range(2, int(sqrt_num+1)):
+        if num % i == 0:
+            return False
+    return True
+
 cnt = 0
-
-def is_Prime(num):
-    if num == 1:
-        return False
-    else:
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                return False
-        else:
-            return True
-
-for num in arr:
-    if is_Prime(num):
+for i in arr:
+    if i > 1 and check_prime(i):
         cnt += 1
 
 print(cnt)
