7114732269c69015f6d5c3211a2b61d2202ec6f6
Seok <82555245+Ui-Seok@users.noreply.github.com>
2023-08-22 00:14:52 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,29 @@
+n = int(input())
+cnt = 0
+
+def check_num(nums):
+    if n % 3 == 0:
+        answer = n // 3
+        if answer >= 5:
+            cnt = answer // 5
+            answer = cnt * 3 + answer % 5
+    elif n % 5 == 0:
+        answer = n // 5
+    else:
+        answer = False
+        
+    return answer
+
+while n > 0:
+    if check_num(n) == False:
+        n -= 5
+        cnt += 1
+    else:
+        answer = check_num(n)
+        cnt += answer
+        break
+    
+if n < 0:
+    print('-1')
+else:
+    print(cnt)
