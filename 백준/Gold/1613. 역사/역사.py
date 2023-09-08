b352aabd99137d55fff70b617a15135229ad503f
Seok <82555245+Ui-Seok@users.noreply.github.com>
2023-09-08 17:55:44 +0900

    [Gold III] Title: 역사, Time: 748 ms, Memory: 117372 KB -BaekjoonHub

--- 백준/Gold/1613. 역사/역사.py
+++ 백준/Gold/1613. 역사/역사.py
@@ -1,56 +1,22 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-graph = {}
+
+graph = [[0] * (n + 1) for _ in range(n + 1)]
 for _ in range(k):
     a, b = map(int, input().split())
-    if graph.get(a):
-        graph[a].append(b)
-    else:
-        graph[a] = [b]
+    graph[a][b] = 1
+
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if graph[i][k] and graph[k][j]:
+                graph[i][j] = 1
 
 s = int(input())
 for _ in range(s):
-    c, d = map(int, input().split())
-    answer = 0
-
-    q = deque()
-    q.append(c)
-    
-    visit = [False] * (n + 1)
-    visit[c] = True
-
-    while q:
-        x = q.popleft()
-        if x == d:
-            answer = -1
-            break
-        
-        if graph.get(x):
-            for i in graph[x]:
-                if not visit[i]:
-                    visit[i] = True
-                    q.append(i)
-
-    else:
-        q = deque()
-        q.append(d)
-
-        visit = [False] * (n + 1)
-        visit[d] = True
-
-        while q:
-            x = q.popleft()
-            if x == c:
-                answer = 1
-                break
-
-            if graph.get(x):
-                for i in graph[x]:
-                    if not visit[i]:
-                        visit[i] = True
-                        q.append(i)
-
-    print(answer)
+    x, y = map(int, input().split())
+    if graph[x][y]: print(-1)
+    elif graph[y][x]: print(1)
+    else: print(0)
