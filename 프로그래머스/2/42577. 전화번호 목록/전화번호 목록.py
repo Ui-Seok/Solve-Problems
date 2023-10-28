532ec2a3509a97ebff0ba490908307f717df330c
Seok <82555245+Ui-Seok@users.noreply.github.com>
2023-10-28 23:39:26 +0900

    [level 2] Title: 전화번호 목록, Time: 79.58 ms, Memory: 28 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,10 @@
+def solution(phone_book):
+    answer = True
+    phone_book = sorted(phone_book)
+    
+    for a, b in zip(phone_book, phone_book[1:]):
+        if b.startswith(a):
+            answer = False
+            break
+
+    return answer
